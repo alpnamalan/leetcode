255a8fd446e4bb0bae0371ddb6da46276e5c4590
Alp Namalan <144292057+alpnamalan@users.noreply.github.com>
2025-09-05 20:35:22 +0900

    Implement MedianFinder using two heaps

--- heap/hard1.py
+++ heap/hard1.py
@@ -1,26 +1,26 @@
 ### AUG 10, 2025 -- P295: FIND MEDIAN FROM DATA STREAM ###
 
-class Solution:
-    def longestPalindrome(self, words: List[str]) -> int:
-        from collections import Counter
-        ctr = Counter(words)
-        middle = False
-        length = 0
-        for syl in list(ctr.keys()):
-            comp = syl[1]+syl[0]
-            if comp == syl: # either same or another copy
-                incr = ctr[syl] // 2
-                length += incr*4
-                ctr[syl] -= incr*2
-                if ctr[syl] == 1:
-                    middle = True
-                    ctr[syl] -= 1
-            elif comp in ctr: 
-                incr = min(ctr[syl], ctr[comp])
-                length += incr*4
-                ctr[syl] -= incr
-                # if ctr[syl] == 0: del ctr[syl]
-                ctr[comp] -= incr
-                # if ctr[comp] == 0: del ctr[comp]
-        if middle: length += 2
-        return length
+class MedianFinder:
+    import heapq
+    """TWO HEAP APPROACH"""
+    def __init__(self):
+        self.lhp = [] # max heap for <= med
+        self.rhp = [] # min heap for >= med
+        
+    def addNum(self, num: int) -> None:
+        # convention: when odd, keep the extra in left heap
+        # first push to left
+        heapq.heappush(self.lhp, -num)
+        # ensure left[0] ≤ right[0]
+        if self.rhp and -self.lhp[0] > self.rhp[0]:
+            heapq.heappush(self.rhp, -heapq.heappop(self.lhp))
+        # balance sizes; len(lhp) can be len(rhp)+1
+        if len(self.lhp) > len(self.rhp)+1:
+            heapq.heappush(self.rhp, -heapq.heappop(self.lhp))
+        elif len(self.rhp) > len(self.lhp): # right CAN'T be larger
+            heapq.heappush(self.lhp, -heapq.heappop(self.rhp))
+
+    def findMedian(self) -> float:
+        if len(self.lhp) == len(self.rhp):
+            return (self.rhp[0]-self.lhp[0]) / 2.0
+        else: return float(-self.lhp[0]) # median in the left heap
